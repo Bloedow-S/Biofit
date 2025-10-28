f0692be19a53a05e0952cfec474e1fdfa5f7d6ed
Sabrina Bloedow <sabrinabloedow80@gmail.com>
2025-10-28 11:18:58 -0300

    Adicionando login, css e html

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Login from './pages/Login'
+import './index.css'    //estilos globais, aplicado em todo o programa
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Login />
   </StrictMode>,
 )
